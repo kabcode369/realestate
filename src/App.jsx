828d979ac5124a035decf745dec4356f3cc88445
Kabir <kabirhamidani@gmail.com>
2023-10-20 07:13:57 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Aboutus from './pages/Aboutus';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import SignOut from './pages/SignOut';
+import SignIn from "./pages/SignUp";
+
 
 export default function App() {
-  return <h1 className='text-red-500'>App</h1>
+  return (
+    <BrowserRouter>
+
+    <Routes>
+      <Route path ='/'element={<Home />} />
+      <Route path ='/SignUp' element={<SignUp />} />
+      <Route path ='/SignOut' element={<SignOut />} />
+      <Route path ='/Profile' element={<Profile />} />
+    </Routes>
+
+    </BrowserRouter>
+  )
 }
